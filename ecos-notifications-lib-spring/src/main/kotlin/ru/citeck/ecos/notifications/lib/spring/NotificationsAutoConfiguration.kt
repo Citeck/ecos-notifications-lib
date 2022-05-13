862fd558c975f504acda3a229887e92cd2290928
Pavel Simonov <pavel.simonov@citeck.ru>
2022-05-13 13:38:54 +0700

    ECOSCOM-4664 - ECOS WebApp interfaces

--- ecos-notifications-lib-spring/src/main/kotlin/ru/citeck/ecos/notifications/lib/spring/NotificationsAutoConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package ru.citeck.ecos.notifications.lib.spring
-
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@ComponentScan(basePackages = ["ru.citeck.ecos.notifications.lib.spring"])
-open class NotificationsAutoConfiguration
